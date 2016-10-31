33417dc4f603415c152313344e160e9b0e53cc44
Daryl Antony <daryl@commoncode.io>
2016-11-01 10:28:37 +1100

    these are not needed

--- PeopleList/PeopleList/userList/models.py
+++ PeopleList/PeopleList/userList/models.py
@@ -1,10 +1,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
-import datetime
 from django.utils import timezone
 
-# Create your models here.
 
 class Skill(models.Model):
     name = models.CharField(max_length=50)
